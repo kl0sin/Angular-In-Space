21a4b352e5a52ce745f79505e9baedf934cafb6b
Mateusz Kłosiński <mkklosin@gmail.com>
2018-08-22 10:15:00 +0200

    Pilot added

--- src/app/space/scout-2-ship.ts
+++ src/app/space/scout-2-ship.ts
@@ -1,8 +1,9 @@
 import { SpaceShip } from './space-ship';
+import { Pilot } from './pilot';
 
 export class Scout2Ship extends SpaceShip {
-  constructor() {
-    super('Scout 2', '/assets/pictures/ships/scout-2.png');
+  constructor(pilot?: Pilot) {
+    super('Scout 2', '/assets/pictures/ships/scout-2.png', pilot);
     this.shipHealth = 50;
     this.shipWeaponActive = false;
     this.shipShieldActive = false;
--- src/app/space/space-ship.ts
+++ src/app/space/space-ship.ts
@@ -1,3 +1,5 @@
+import { Pilot } from './pilot';
+
 export abstract class SpaceShip {
   shipModel: string;
   shipImage: string;
@@ -6,11 +8,13 @@ export abstract class SpaceShip {
   shipWeaponActive: boolean;
   shipWeaponImage: string;
   shipShieldImage: string;
+  pilot: Pilot;
 
-  constructor(shipModel: string, shipImage: string) {
+  constructor(shipModel: string, shipImage: string, pilot: Pilot) {
     this.shipWeaponImage = '/assets/pictures/ships/weapon.png';
     this.shipShieldImage = '/assets/pictures/ships/shield.png';
     this.shipModel = shipModel;
     this.shipImage = shipImage;
+    this.pilot = pilot;
   }
 }
