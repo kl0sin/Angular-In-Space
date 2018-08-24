5eb0dfb1aeddf2f5788f7655b842d0008acb6fd6
Mateusz Kłosiński <mkklosin@gmail.com>
2018-08-24 14:30:14 +0200

    router added

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
+import { HangarComponent } from '../hangar/hangar.component';
+
+const routes: Routes = [
+  { path: '', component: HangarComponent, pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [
     CommonModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forChild(routes)
   ],
   exports: [
     FormsModule
